4f346a803e5a0786c460df64d27fc4a1d9612564
Muralee <soberbutcrazy@gmail.com>
2022-06-17 22:54:04 +0200

    New

---
+++ Desktop/Coding Practice/Amazone290322/Amazone290322/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Amazone290322")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Amazone290322")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e258bd6f-7dae-4230-b8b8-16036333f7d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Desktop/Coding Practice/Amazone290322/Amazone290322/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace Amazone290322
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Driver Initialized, Url opened and Cookies accepted
+            IWebDriver webdriver = InitializeDriverGoToUrlAndAcceptCookies();
+            Thread.Sleep(2000);
+
+            //Searching for the first Item "X box"
+            string object1 = "X Box 512GB";
+            webdriver.FindElement(By.Id("twotabsearchtextbox")).SendKeys(object1);
+            webdriver.FindElement(By.Id("nav-search-submit-button")).Click();
+            webdriver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span")).Click();
+            webdriver.FindElement(By.Name("submit.add-to-cart")).Click();
+            webdriver.SwitchTo().Window(webdriver.WindowHandles[1]);
+            webdriver.FindElement(By.Id("attachSiNoCoverage-announce")).Click();
+            //IWebElement nothanksTab = webdriver.FindElement(By.ClassName("a-button a-button-grouplast a-button-base"));
+            //actions.MoveToElement(nothanksTab).Build().Perform();
+            //actions.MoveToElement(nothanksTab).Click();
+
+            Thread.Sleep(2000);
+        }
+
+         public IWebDriver InitializeDriverGoToUrlAndAcceptCookies()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.amazon.de/");
+            string acceptCookiesPath = "#sp-cc-accept";
+            driver.FindElement(By.CssSelector(acceptCookiesPath)).Click();
+            return driver;
+        }
+
+        //public we SearchAndAddToCart(object1);
+        //{
+
+        //}
+    }
+}
